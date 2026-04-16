e15cfcb61cda1eddafb7d7e5a01af622d49bc593
Nancy Wei <nancywei20030416@gmail.com>
2026-04-16 13:17:08 +1000

    init micro service

---
+++ Order/src/main/java/org/target/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package org.target.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
---
+++ Payment/src/test/java/org/target/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.target.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
