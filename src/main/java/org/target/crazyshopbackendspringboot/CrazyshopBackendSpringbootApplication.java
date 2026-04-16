d6e2730cfd804b268b80b435794f8737fafc1b80
Nancy Wei <nancywei20030416@gmail.com>
2026-04-16 12:50:24 +1000

    init

---
+++ src/main/java/org/target/crazyshopbackendspringboot/CrazyshopBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.target.crazyshopbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyshopBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyshopBackendSpringbootApplication.class, args);
+    }
+
+}
